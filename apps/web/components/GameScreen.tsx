f7b4566e402b31a6d94bb54f0772ec26aaad3ccd
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-05-02 15:03:41 +0530

    small ui change fix or update

--- apps/web/components/GameScreen.tsx
+++ apps/web/components/GameScreen.tsx
@@ -1,23 +1,9 @@
-"use client";
-import { GameEvents, StartGame, States } from "@repo/common";
-import { useAppContext } from "../app/context";
 import GameHeader from "./GameHeader";
 import Scoreboard from "./Scoreboard";
 import Chats from "./Chats";
 import Drawboard from "./Drawboard";
 
 const GameScreen = () => {
-  const { game, me, socket } = useAppContext();
-
-  const handleStart = () => {
-    if (!socket || !game) return;
-    const data: StartGame = {
-      type: GameEvents.START_GAME,
-      gameId: game.gameId,
-    };
-    socket.send(JSON.stringify(data));
-  };
-
   return (
     <div className="h-screen p-16 space-y-2">
       <div className="grid grid-cols-12 gap-2">
@@ -37,25 +23,6 @@ const GameScreen = () => {
           <Chats />
         </div>
       </div>
-      {game?.gameState.state === States.NOT_STARTED ? (
-        <div className="flex max-w-3xl mx-auto justify-center gap-2">
-          <button
-            className="flex-1 text-4xl bg-blue-400 rounded-md p-1 border-2 border-black"
-            disabled={game?.creator.id !== me?.id}
-            onClick={handleStart}
-          >
-            Start
-          </button>
-          <button
-            className="flex-1 text-4xl bg-green-500 rounded-md p-1 border-2 border-black"
-            onClick={() =>
-              navigator.clipboard.writeText(`http://localhost:3000/?game=${game?.gameId}`)
-            }
-          >
-            Invite
-          </button>
-        </div>
-      ) : null}
     </div>
   );
 };
--- apps/web/components/overlays/SettingOverlay.tsx
+++ apps/web/components/overlays/SettingOverlay.tsx
@@ -1,23 +1,58 @@
+"use client";
 import { GameSettings } from "@repo/common/types";
 import React from "react";
+import { useAppContext } from "../../app/context";
+import { GameEvents, StartGame } from "@repo/common";
 
 const SettingOverlay = ({ settings }: { settings: GameSettings }) => {
-  return (
-    <div className="inset-0 absolute bg-neutral-400 space-y-2 p-4">
-      <h2 className="text-3xl font-bold">Game Settings</h2>
+  const { game, me, socket } = useAppContext();
 
+  const handleStart = () => {
+    if (!socket || !game) return;
+    const data: StartGame = {
+      type: GameEvents.START_GAME,
+      gameId: game.gameId,
+    };
+    socket.send(JSON.stringify(data));
+  };
+  return (
+    <div className="inset-0 absolute bg-neutral-400 p-4 flex flex-col items-center">
+      <h2 className="text-3xl font-bold">Game Settings</h2>
+        <div className="flex flex-col h-full justify-around">
+
+       
       <div className="grid grid-cols-2 gap-4 text-xl">
         <div className="font-bold">Rounds:</div>
-        <div>{settings.rounds}</div>
+        <div className="text-center">{settings.rounds}</div>
 
         <div className="font-bold">Max Players:</div>
-        <div>{settings.totalPlayers}</div>
+        <div className="text-center">{settings.totalPlayers}</div>
 
         <div className="font-bold">Draw Time (sec):</div>
-        <div>{settings.drawTime}</div>
+        <div className="text-center">{settings.drawTime}</div>
 
         <div className="font-bold">Word Select Time (sec):</div>
-        <div>{settings.wordSelectTime}</div>
+        <div className="text-center">{settings.wordSelectTime}</div>
+      </div>
+
+      <div className="flex gap-2">
+        <button
+          className="flex-1 text-4xl bg-blue-400 rounded-md p-1 border-2 border-black"
+          disabled={game?.creator.id !== me?.id}
+          onClick={handleStart}
+        >
+          Start
+        </button>
+        <button
+          className="flex-1 text-4xl bg-green-500 rounded-md p-1 border-2 border-black"
+          onClick={() =>
+            navigator.clipboard.writeText(`http://localhost:3000/?game=${game?.gameId}`)
+          }
+        >
+          Invite
+        </button>
+      </div>
+
       </div>
     </div>
   );
