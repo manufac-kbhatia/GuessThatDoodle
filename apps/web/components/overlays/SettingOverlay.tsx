f92556b9d741fa45b5a5c3701a6bff2ee5159460
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-05-05 10:40:46 +0530

    minor

--- apps/web/components/overlays/SettingOverlay.tsx
+++ apps/web/components/overlays/SettingOverlay.tsx
@@ -44,13 +44,13 @@ const SettingOverlay = ({ settings }: { settings: GameSettings }) => {
           <button
             className="flex-1 text-4xl bg-green-500 rounded-md p-1 border-2 border-black cursor-pointer transition active:scale-95"
             onClick={() =>
-              navigator.clipboard.writeText(`http://localhost:3000/?game=${game?.gameId}`)
+              navigator.clipboard.writeText(`https://guessthatdoodle.vercel.app/?game=${game?.gameId}`)
             }
           >
             Invite
           </button>
         </div>
-      </div>
+      </div>ˀ
     </div>
   );
 };
